d0e13917b050d3b4f589f0bab27e1315b60c3f83
wooo <fotoz01@gmail.com>
2016-12-20 22:07:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+var mic;
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  mic = new p5.AudioIn();
+  mic.start();
+
+}
+
+function draw() {
+  background(100);
+  //var volume = map (mic.getLevel(), 0, 1, height, 0);
+  var volume = map (mic.getLevel(), 0, 1, 0, height);
+  console.log(volume);
+  
+  //ellipse (width/2, volume, 20, 20);
+  //ellipse (width/2, height/2, 50, volume);
+  fill(255, volume, volume/2)
+ellipse (width/2, height/2, 200, 200);
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    song.pause();
+  }
+  else  {
+    //song.play();
+    song.loop();
+  }
+
+}
